6977e4e3f563f1f574d8810fd4d7dde8ba1566c1
KatiaRassadina <rassadinaekaterina877@gmail.com>
2022-11-30 00:48:42 +0300

    new file

--- Program.cs
+++ Program.cs
@@ -1,22 +1,60 @@
 ﻿// See 
 int size = 8;
+int n = 3;
 string[] arrayFirst = new string[size];
 Console.WriteLine($"Input {size} elemets of massive");
 FillArray(arrayFirst);
 PrintArray(arrayFirst);
+Console.WriteLine($"New massive with elements <= {n}:");
+Console.WriteLine();
+if (FindSizeSecondArray(arrayFirst) == 0)
+{
+    Console.WriteLine("Nothing suitable for new massive");
+}
+else
+{
+    string[] arraySecond = TransferElements(arrayFirst);
+    PrintArray(arraySecond);
+}
 void FillArray (string[] arr)
 {
     for(int i = 0; i < size; i++)
     {
-        arr[i] = Console.ReadLine() ?? " ";
+        arr[i] = Console.ReadLine();
     }
 }
 void PrintArray(string[] arr)
 {
+    int k = arr.Length;
     Console.Write("[");
-    for(int i = 0; i < size; i++)
+    for(int i = 0; i < k; i++)
     {
         Console.Write(arr[i] + " ");
     }
     Console.Write("]");
+}
+int FindSizeSecondArray(string[] arr)
+{
+    int SecondSize = 0;
+    for(int i = 0; i < size; i++)
+    {
+        if(arr[i].Length <= n)
+       {
+            SecondSize++;
+       }   
+    }
+    return SecondSize;
+}
+string[] TransferElements(string[] arr)
+{
+    string[] arraySecond = new string[FindSizeSecondArray(arrayFirst)];
+       for(int i = 0, j = 0; i < size; i++)
+    {
+        if(arr[i].Length <= n)
+       {
+            arraySecond[j] = arr[i];
+            j++;
+       }   
+    }
+return arraySecond;
 }
