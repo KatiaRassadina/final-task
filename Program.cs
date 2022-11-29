87bfd3ca21496505d814c7ebdd9befc423a688a5
KatiaRassadina <rassadinaekaterina877@gmail.com>
2022-11-29 22:48:54 +0300

    new file

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See 
+int size = 8;
+string[] arrayFirst = new string[size];
+Console.WriteLine($"Input {size} elemets of massive");
+FillArray(arrayFirst);
+PrintArray(arrayFirst);
+void FillArray (string[] arr)
+{
+    for(int i = 0; i < size; i++)
+    {
+        arr[i] = Console.ReadLine() ?? " ";
+    }
+}
+void PrintArray(string[] arr)
+{
+    Console.Write("[");
+    for(int i = 0; i < size; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.Write("]");
+}
